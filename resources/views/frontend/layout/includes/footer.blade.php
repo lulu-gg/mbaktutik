cd2e922d0b1402ed6c8d7654e62df3fcd2cfd484
fairuzminannafis <fairuz.nafiz@ist.id>
2024-05-31 05:36:16 +0700

    adjust footer

--- resources/views/frontend/layout/includes/footer.blade.php
+++ resources/views/frontend/layout/includes/footer.blade.php
@@ -1,3 +1,9 @@
+@php
+
+    $generalParameter = \App\Models\GeneralParamter::first();
+
+@endphp
+
 <div class="cs-height_100 cs-height_lg_70"></div>
 <footer class="cs-footer cs-style1">
     <div class="cs-footer_bg"></div>
@@ -8,8 +14,10 @@
                 <div class="row">
                     <div class="col-lg-4 col-sm-4">
                         <div class="cs-footer_widget">
-                            <img src="" style="width:200px;" alt="">
-                            <h4>Beli Tiket Lebih Mudah</h4>
+                            <img src="{{ asset('images/logo-white.png') }}" style="width:200px;" alt="">
+                            <div class="mt-4">
+                                <h4>Beli Tiket Lebih Mudah</h4>
+                            </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-sm-4">
@@ -26,9 +34,13 @@
                         <div class="cs-footer_widget">
                             <h2 class="cs-widget_title">Kontak Kami</h2>
                             <ul class="cs-widget_nav">
-                                <li>Alamat</li>
-                                <li>Email</li>
-                                <li>WhatsApp</li>
+                                <li>{{ $generalParameter->address }}</li>
+                                <li>
+                                    <a href="mailto:{{ $generalParameter->email }}">{{ $generalParameter->email }}</a>
+                                </li>
+                                <li>
+                                    <a href="{!! $generalParameter->whatsapp_url !!}">Whatsapp</a>
+                                </li>
                             </ul>
                         </div>
                     </div>
