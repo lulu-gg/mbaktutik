6af12ffd2e461ea356d011750469524d1c9529e6
fairuzminannafis <fairuz.nafiz@ist.id>
2024-06-02 05:56:03 +0700

    dark theme on login

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 
-<html lang="en" class="light-style  customizer-hide" dir="ltr" data-theme="theme-default"
-    data-assets-path="../sneat-pro/assets/" data-template="vertical-menu-template">
+<html lang="en" class="dark-style layout-menu-fixed" dir="ltr" data-theme="theme-semi-dark"
+    data-assets-path="{{ asset('sneat-pro/assets/') }}/" data-template="vertical-menu-template">
 
 <head>
     <meta charset="utf-8" />
@@ -21,33 +21,35 @@
         rel="stylesheet">
 
     <!-- Icons -->
-    <link rel="stylesheet" href="../sneat-pro/assets/vendor/fonts/boxicons.css" />
-    <link rel="stylesheet" href="../sneat-pro/assets/vendor/fonts/fontawesome.css" />
-    <link rel="stylesheet" href="../sneat-pro/assets/vendor/fonts/flag-icons.css" />
+    <link rel="stylesheet" href="{{ asset('sneat-pro/assets/vendor/fonts/boxicons.css') }}" />
+    <link rel="stylesheet" href="{{ asset('sneat-pro/assets/vendor/fonts/fontawesome.css') }}" />
+    <link rel="stylesheet" href="{{ asset('sneat-pro/assets/vendor/fonts/flag-icons.css') }}" />
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href="../sneat-pro/assets/vendor/css/rtl/core.css" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="../sneat-pro/assets/vendor/css/rtl/theme-default.css"
+    <link rel="stylesheet" href="{{ asset('sneat-pro/assets/vendor/css/rtl/core.css') }}"
+        class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('sneat-pro/assets/vendor/css/rtl/theme-default.css') }}"
         class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="../sneat-pro/assets/css/demo.css" />
+    <link rel="stylesheet" href="{{ asset('sneat-pro/assets/css/demo.css') }}" />
 
     <!-- Vendors CSS -->
-    <link rel="stylesheet" href="../sneat-pro/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
-    <link rel="stylesheet" href="../sneat-pro/assets/vendor/libs/typeahead-js/typeahead.css" />
+    <link rel="stylesheet" href="{{ asset('sneat-pro/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+    <link rel="stylesheet" href="{{ asset('sneat-pro/assets/vendor/libs/typeahead-js/typeahead.css') }}" />
     <!-- Vendor -->
-    <link rel="stylesheet" href="../sneat-pro/assets/vendor/libs/formvalidation/dist/css/formValidation.min.css" />
+    <link rel="stylesheet"
+        href="{{ asset('sneat-pro/assets/vendor/libs/formvalidation/dist/css/formValidation.min.css') }}" />
 
     <!-- Page CSS -->
     <!-- Page -->
-    <link rel="stylesheet" href="../sneat-pro/assets/vendor/css/pages/page-auth.css">
+    <link rel="stylesheet" href="{{ asset('sneat-pro/assets/vendor/css/pages/page-auth.css') }}">
     <!-- Helpers -->
-    <script src="../sneat-pro/assets/vendor/js/helpers.js"></script>
+    <script src="{{ asset('sneat-pro/assets/vendor/js/helpers.js') }}"></script>
 
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
     <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
-    <script src="../sneat-pro/assets/vendor/js/template-customizer.js"></script>
+    <script src="{{ asset('sneat-pro/assets/vendor/js/template-customizer.js') }}"></script>
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="../sneat-pro/assets/js/config.js"></script>
+    <script src="{{ asset('sneat-pro/assets/js/config.js') }}"></script>
 </head>
 
 <body>
@@ -112,28 +114,28 @@
 
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
-    <script src="../sneat-pro/assets/vendor/libs/jquery/jquery.js"></script>
-    <script src="../sneat-pro/assets/vendor/libs/popper/popper.js"></script>
-    <script src="../sneat-pro/assets/vendor/js/bootstrap.js"></script>
-    <script src="../sneat-pro/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="{{ asset('sneat-pro/assets/vendor/libs/jquery/jquery.js') }}"></script>
+    <script src="{{ asset('sneat-pro/assets/vendor/libs/popper/popper.js') }}"></script>
+    <script src="{{ asset('sneat-pro/assets/vendor/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('sneat-pro/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
 
-    <script src="../sneat-pro/assets/vendor/libs/hammer/hammer.js"></script>
-    <script src="../sneat-pro/assets/vendor/libs/i18n/i18n.js"></script>
-    <script src="../sneat-pro/assets/vendor/libs/typeahead-js/typeahead.js"></script>
+    <script src="{{ asset('sneat-pro/assets/vendor/libs/hammer/hammer.js') }}"></script>
+    <script src="{{ asset('sneat-pro/assets/vendor/libs/i18n/i18n.js') }}"></script>
+    <script src="{{ asset('sneat-pro/assets/vendor/libs/typeahead-js/typeahead.js') }}"></script>
 
-    <script src="../sneat-pro/assets/vendor/js/menu.js"></script>
+    <script src="{{ asset('sneat-pro/assets/vendor/js/menu.js') }}"></script>
     <!-- endbuild -->
 
     <!-- Vendors JS -->
-    <script src="../sneat-pro/assets/vendor/libs/formvalidation/dist/js/FormValidation.min.js"></script>
-    <script src="../sneat-pro/assets/vendor/libs/formvalidation/dist/js/plugins/Bootstrap5.min.js"></script>
-    <script src="../sneat-pro/assets/vendor/libs/formvalidation/dist/js/plugins/AutoFocus.min.js"></script>
+    <script src="{{ asset('sneat-pro/assets/vendor/libs/formvalidation/dist/js/FormValidation.min.js') }}"></script>
+    <script src="{{ asset('sneat-pro/assets/vendor/libs/formvalidation/dist/js/plugins/Bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('sneat-pro/assets/vendor/libs/formvalidation/dist/js/plugins/AutoFocus.min.js') }}"></script>
 
     <!-- Main JS -->
-    <script src="../sneat-pro/assets/js/main.js"></script>
+    <script src="{{ asset('sneat-pro/assets/js/main.js') }}"></script>
 
     <!-- Page JS -->
-    {{-- <script src="../sneat-pro/assets/js/pages-auth.js"></script> --}}
+    {{-- <script src="{{ asset('') }}sneat-pro/assets/js/pages-auth.js"></script> --}}
 
 </body>
 
